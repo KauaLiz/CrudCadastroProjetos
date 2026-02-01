e6458f852c3466469d9327ef18ea9d2b9219a3d4
Kauã de Liz <kauadeliz@gmail.com>
2026-02-01 15:18:37 -0300

    test: setup initial environment test and applicattion-test

--- src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
+++ src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
@@ -1,4 +1,55 @@
+package com.example.cadastroProjetos.service;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 class ProjetoServiceTest {
-  
+
+    @Test
+    void validarMembroIndividual() {
+    }
+
+    @Test
+    void validarGerenteEMembro() {
+    }
+
+    @Test
+    void membroPodeSerAlocado() {
+    }
+
+    @Test
+    void criar() {
+    }
+
+    @Test
+    void retornarDadosRelatorio() {
+    }
+
+    @Test
+    void transformarDto() {
+    }
+
+    @Test
+    void mostrarProjetos() {
+    }
+
+    @Test
+    void adicionarMembros() {
+    }
+
+    @Test
+    void retornaProximoStatus() {
+    }
+
+    @Test
+    void avancarStatus() {
+    }
+
+    @Test
+    void cancelarProjeto() {
+    }
+
+    @Test
+    void deletarProjeto() {
+    }
 }
