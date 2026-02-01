34a368dc9d08142e61f24ba6bd84f3038d544229
Kauã de Liz <kauadeliz@gmail.com>
2026-02-01 17:01:15 -0300

    test: ValidarMembroIndividual throws exceptions

--- src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
+++ src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
@@ -1,12 +1,73 @@
 package com.example.cadastroProjetos.service;
 
+import com.example.cadastroProjetos.customException.RecursoNaoEncontradoException;
+import com.example.cadastroProjetos.customException.RegraNegocioException;
+import com.example.cadastroProjetos.model.dto.MembroDto;
+import com.example.cadastroProjetos.model.entity.ProjetoEntity;
+import com.example.cadastroProjetos.model.enums.Status;
+import com.example.cadastroProjetos.repository.ProjetoRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+
 class ProjetoServiceTest {
 
+    @Mock
+    private ProjetoRepository repository;
+
+    @Mock
+    private MembroApiMockada membroApiMockada;
+
+    @InjectMocks
+    private ProjetoService projetoService;
+
+    @BeforeEach
+    void setup(){
+        MockitoAnnotations.initMocks(this);
+    }
+
     @Test
-    void validarMembroIndividual() {
+    @DisplayName("Deve retornar exceção referente a membro não encontrado")
+    void validarMembroNaoEncontrado() {
+        when(membroApiMockada.consultarID(1L)).thenReturn(null);
+
+        assertThrows(RecursoNaoEncontradoException.class, () ->{
+            projetoService.validarMembroIndividual(1L);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar exceção referente ao cargo estar diferente de funcionário")
+    void validarMembroCargoErrado() {
+        when(membroApiMockada.consultarID(1L)).thenReturn(new MembroDto("Kauã", "Gerente"));
+
+        assertThrows(RegraNegocioException.class, () ->{
+            projetoService.validarMembroIndividual(1L);
+        });
+    }
+
+    @Test
+    @DisplayName("Deve retornar exceção se membro já estiver em pelo menos 3 projetos")
+    void validarMembroLimiteProjetos() {
+        when(membroApiMockada.consultarID(1L)).thenReturn(new MembroDto("Kauã", "Funcionário"));
+
+        when(repository.contarProjetosMembroAtivo(1L, List.of(Status.CANCELADO, Status.ENCERRADO))).thenReturn(3L);
+
+        assertThrows(RegraNegocioException.class, () ->{
+            projetoService.validarMembroIndividual(1L);
+        });
     }
 
     @Test
