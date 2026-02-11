b359c93047e3bb9b20fc7128d9125273aa130257
Kauã de Liz <kauadeliz@gmail.com>
2026-02-11 19:54:50 -0300

    test: add a test for the limit of members per project

--- src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
+++ src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
@@ -73,7 +73,54 @@ class ProjetoServiceTest {
     }
 
     @Test
-    void validarGerenteEMembro() {
+    @DisplayName("Deve retornar exceção quando passar do limite de 10 membros")
+    void validarLimiteDeMembrosNoProjeto(){
+         List<Long> membrosId = List.of(1L,2L,3L,4L,5L,6L,7L,8L,9L,10L,11L);
+
+         assertThrows(ValidacaoException.class, () -> {
+             projetoService.validarQuantidadeMembros(membrosId);
+         });
+    }
+
+    @Test
+    @DisplayName("Deve retonar exceção quando não houver um gerente existente")
+    void validarGerenteNaoEncontrado() {
+        ProjetoDto projetoDto = new ProjetoDto(
+                "ProjetoTeste",
+                LocalDate.now(),
+                LocalDate.now().plusMonths(2),
+                null,
+                new BigDecimal("500"),
+                "ProjetoTeste",
+                1L,
+                null
+        );
+
+        when(membroApiMockada.consultarID(1L)).thenReturn(null);
+
+        assertThrows(RecursoNaoEncontradoException.class, () ->{
+            projetoService.validarGerente(projetoDto, null);
+        });
+    }
+
+    @Test
+    void validarMembroNaoEGerente() {
+
+    }
+
+    @Test
+    void validarGerenteNaoEMembro() {
+
+    }
+
+    @Test
+    void validarQuantidadeInvalidaMembros() {
+
+    }
+
+    @Test
+    void validarMembrosRepetidos() {
+
     }
 
     @Test
