ee4ff9128011ae21c52264bb6eabb373a126e003
Kauã de Liz <kauadeliz@gmail.com>
2026-02-24 20:32:09 -0300

    test: throw exception when trying to delete a finished project

--- src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
+++ src/test/java/com/example/cadastroProjetos/service/ProjetoServiceTest.java
@@ -808,4 +808,20 @@ class ProjetoServiceTest {
         verify(repository).findById(projetoID);
         verify(repository, never()).delete(any());
     }
+
+    @Test
+    @DisplayName("Deve retornar exceção quando projeto estiver com status encerrado")
+    void deletarProjetoFalha_ProjetoEncerrado() {
+        Long projetoID = 1L;
+        ProjetoEntity projetoTeste = new ProjetoEntity();
+        projetoTeste.setStatus(Status.ENCERRADO);
+
+        when(repository.findById(projetoID)).thenReturn(Optional.of(projetoTeste));
+
+        assertThrows(RegraNegocioException.class, () ->{
+            projetoService.deletarProjeto(projetoID);
+        });
+        verify(repository).findById(projetoID);
+        verify(repository, never()).delete(any());
+    }
 }
